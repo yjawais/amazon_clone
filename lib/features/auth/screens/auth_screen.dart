70534ea45ae0fd132174289f02d78ae0d363a1ae
yjawais <yjawais@gmail.com>
2022-06-18 23:35:16 +0530

    routers file added

---
+++ lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  static const String routeName='/auth-screen';
+
+  const AuthScreen({Key? key}) : super(key: key);
+  
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/router.dart
@@ -0,0 +1,19 @@
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreen.routeName:
+      return MaterialPageRoute(
+        builder: (_) => const AuthScreen(),
+      );
+    default:
+      return MaterialPageRoute(
+        builder: (_) => const Scaffold(
+          body: Center(
+            child: Text("Page not found!"),
+          ),
+        ),
+      );
+  }
+}
