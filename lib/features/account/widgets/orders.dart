7904005a1a0d9f3b64f043f19bd955bcc3efbd4a
yjawais <yjawais@gmail.com>
2022-07-28 01:43:49 +0530

    Account Screen Completed

---
+++ lib/features/account/widgets/orders.dart
@@ -0,0 +1,77 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/account/widgets/single_product.dart';
+import 'package:flutter/material.dart';
+
+class Orders extends StatefulWidget {
+  const Orders({Key? key}) : super(key: key);
+
+  @override
+  _OrdersState createState() => _OrdersState();
+}
+
+class _OrdersState extends State<Orders> {
+//temp list
+  List list = [
+    {
+      'https://images.unsplash.com/photo-1649859398021-afbfe80e83b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
+      'https://images.unsplash.com/photo-1649859398021-afbfe80e83b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
+      'https://images.unsplash.com/photo-1649859398021-afbfe80e83b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
+      'https://images.unsplash.com/photo-1649859398021-afbfe80e83b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                left: 15,
+              ),
+              child: const Text(
+                'Your Orders',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                right: 15,
+              ),
+              child: Text(
+                'See all',
+                style: TextStyle(
+                  color: GlobalVariables.selectedNavBarColor,
+                ),
+              ),
+            ),
+
+            //display all order
+          ],
+        ),
+        Container(
+          height: 170,
+          padding: const EdgeInsets.only(
+            left: 10,
+            top: 20,
+            right: 0,
+          ),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: list.length,
+            itemBuilder: (context, index) {
+              return SingleProduct(
+                image: list[index],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
