71d0626b0d631e8af498d163603fa564b1837cd5
yjawais <yjawais@gmail.com>
2022-08-10 18:17:16 +0530

    deal of day added

--- lib/features/home/services/home_services.dart
+++ lib/features/home/services/home_services.dart
@@ -45,36 +45,36 @@ class HomeServices {
     return productList;
   }
 
-  // Future<Product> fetchDealOfDay({
-  //   required BuildContext context,
-  // }) async {
-  //   final userProvider = Provider.of<UserProvider>(context, listen: false);
-  //   Product product = Product(
-  //     name: '',
-  //     description: '',
-  //     quantity: 0,
-  //     images: [],
-  //     category: '',
-  //     price: 0,
-  //   );
+  Future<Product> fetchDealOfDay({
+    required BuildContext context,
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    Product product = Product(
+      name: '',
+      description: '',
+      quantity: 0,
+      images: [],
+      category: '',
+      price: 0,
+    );
 
-  //   try {
-  //     http.Response res =
-  //         await http.get(Uri.parse('$uri/api/deal-of-day'), headers: {
-  //       'Content-Type': 'application/json; charset=UTF-8',
-  //       'x-auth-token': userProvider.user.token,
-  //     });
+    try {
+      http.Response res =
+          await http.get(Uri.parse('$uri/api/deal-of-day'), headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': userProvider.user.token,
+      });
 
-  //     httpErrorHandle(
-  //       response: res,
-  //       context: context,
-  //       onSuccess: () {
-  //         product = Product.fromJson(res.body);
-  //       },
-  //     );
-  //   } catch (e) {
-  //     showSnackBar(context, e.toString());
-  //   }
-  //   return product;
-  // }
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          product = Product.fromJson(res.body);
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+    return product;
+  }
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -56,4 +56,29 @@ productRouter.post("/api/rate-product", auth, async (req, res) => {
   }
 });
 
+productRouter.get("/api/deal-of-day", auth, async (req, res) => {
+  try {
+    let products = await Product.find({});
+
+    products = products.sort((a, b) => {
+      let aSum = 0;
+      let bSum = 0;
+
+      for (let i = 0; i < a.ratings.length; i++) {
+        aSum += a.ratings[i].rating;
+      }
+
+      for (let i = 0; i < b.ratings.length; i++) {
+        bSum += b.ratings[i].rating;
+      }
+      return aSum < bSum ? 1 : -1;
+    });
+
+    res.json(products[0]);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+
 module.exports = productRouter;
