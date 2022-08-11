899ece06256fdec2dc7f47f317489908d0a8c89d
yjawais <yjawais@gmail.com>
2022-08-11 21:29:16 +0530

    cart screen completed

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/features/account/screens/account_screen.dart';
+import 'package:amazon_clone/features/cart/screens/cart_screen.dart';
 import 'package:amazon_clone/features/home/screens/home_screen.dart';
 import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:badges/badges.dart';
@@ -28,7 +29,7 @@ void updatePage(int page){
 List<Widget> pages=[
 const HomeScreen(),
 const AccountScreen(),
-const Text('cart'),
+const CartScreen(),
 
 ];
 
